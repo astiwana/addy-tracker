ceaf4e472ab5f8318370d745409656ac4d54fa8f
Arman <astiwana@ualberta.ca>
2024-10-07 10:27:29 -0600

    updated header

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,11 @@
+import Header from "@/components/Header"
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <header>
-        <div>
-        <Link href={'/'}>LinkList</Link>
-        <nav>
-          <Link href={'/about'}>About</Link>
-          <Link href={'/about'}>Contact</Link>
-        </nav>
-        </div>
-        <div>
-          <Link href={'/login'}>Sign In</Link>
-          <Link href={'/register'}>Create Account</Link>
-        </div>
-      </header>
+      <Header />  
       <section>
 
       </section>
