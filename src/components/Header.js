13dfc38119e9821755e48e0ad1e421088de70c1d
Arman <astiwana@ualberta.ca>
2024-10-07 12:04:59 -0600

    created login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="bg-white border-b py-4">
-            <div className="max-w-4xl flex justify-between mx-auto">
-                <div className="flex gap-6 px-8">
+            <div className="max-w-4xl flex justify-between mx-auto px-6">
+                <div className="flex gap-6">
                     <Link href={'/'}>LinkList</Link>
                     <nav className="flex items-center gap-4 text-slate-500 text-sm">
                         <Link href={'/about'}>About</Link>
