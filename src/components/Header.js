cc763b06829e65c9a5d6b4743a0f530c44c48291
Arman <astiwana@ualberta.ca>
2024-10-07 12:48:34 -0600

    implemented sign in/sign out functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,40 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import LogoutButton from "./buttons/LogoutButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {
+export default async function Header() {
+    const session = await getServerSession(authOptions);
     return (
         <header className="bg-white border-b py-4">
             <div className="max-w-4xl flex justify-between mx-auto px-6">
-                <div className="flex gap-6">
-                    <Link href={'/'}>LinkList</Link>
+                <div className="flex items-center gap-6">
+                    <Link href={'/'} className="flex items-center gap-2 text-blue-500">
+                    <FontAwesomeIcon icon={faLink} className="text-blue-500"/>
+                    <span>LinkList</span>
+                    </Link>
                     <nav className="flex items-center gap-4 text-slate-500 text-sm">
                         <Link href={'/about'}>About</Link>
                         <Link href={'/contact'}>Contact</Link>
                     </nav>
                 </div>
-                <nav className="flex gap-4 text-sm text-slate-500">
-                    <Link href={'/login'}>Sign In</Link>
-                    <Link href={'/register'}>Create Account</Link>
+                <nav className="flex items-center gap-4 text-sm text-slate-500">
+                    {!!session && (
+                        <>
+                            <Link href={'/account'}>
+                                Hello, {session?.user?.name}
+                            </Link>
+                            <LogoutButton />
+                        </>
+                    )}
+                    {!session && (
+                        <>
+                            <Link href={'/login'}>Sign In</Link>
+                            <Link href={'/register'}>Create Account</Link>
+                        </>
+                    )}
                 </nav>
             </div>
       </header>
